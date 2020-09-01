3f1fcc76c248c3c04a909f880dbed297a17164bc
akshaykp007 <akshaykp2661998@gmail.com>
2020-09-01 13:10:25 +0100

    committing

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const bodyparser =require('body-parser');
 const cors = require('cors');
+const {loginValidation,registerValidation} = require('./validation')
 
 var db = require('knex')({
   client: 'pg',
@@ -24,14 +25,41 @@ app.use(cors());
 
 
 app.post('/signin',(req, res) =>{
+	const {error} = loginValidation(req.body)
+	if(error)
+		return res.status(400).send(error.details[0].message)
 	const {email, password} =req.body;
-	db('signin').insert({
+	db('register').where({
+    email: email,
+    password:  password
+    }).select('user_id')
+    .then(data=>{ 
+    	if(data.length){
+	    	db('signin').insert({
+			email: email,
+			password: password
+		    }).then(res.json('ok'))
+      }
+      else{
+      	return res.status(200).send("no such user")
+      }
+    })
+})
+
+app.post('/register',(req, res) =>{
+	const {error} = registerValidation(req.body)
+	if(error)
+		return res.status(400).send(error.details[0].message)
+	const {email, password,name} =req.body;
+	db('register').insert({
+		username: name,
 		email: email,
 		password: password
-	}).then(res.json('hello'))
-	db.select().table('signin').then(data =>{
-	console.log(data)
+
+	})
+	.then(()=>res.json('ok'))
+	.catch(() => res.json('Already'))
 })
-})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
